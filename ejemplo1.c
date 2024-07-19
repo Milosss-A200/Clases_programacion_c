371e1118c776822732597d12cfeaaa4ffc194a9c
Milosss-A200 <Animatebow@yahoo.com>
2024-07-19 11:17:41 -0600

    Add files via upload

---
+++ ejemplo1.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main () {
+    int a, b, suma, resta, multiplicacion;
+    printf ("ingresar el numero uno:\n");
+    scanf ("%d", &a);
+    printf ("ingresar el numero dos:\n");
+    scanf ("%d", &b);
+    suma = a + b; 
+    printf ("la suma es %d\n", resta);
+    resta = a - b;
+    printf ("La restas es %d\n",resta);
+    multiplicacion = a * b;
+    printf ("la multiplicacion es %d\n",multiplicacion);
+    return 0;
+}
